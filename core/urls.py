8e361e7fa230d6e5f960e131ad57e70282f9ba94
alezamarion <alezamarion@icloud.com>
2024-04-17 23:30:37 -0300

    feat: include index and pong spa view

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
+from pong.views import pong
 
 urlpatterns = [
-	path('', include('pong.urls')),
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+	path('pong/', pong, name='pong_game'),
 ]
--- pong/views.py
+++ pong/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 def pong(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    return render(request, 'pong/pong.html')
